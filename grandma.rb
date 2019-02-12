e8d55545e852af458d67e8a80e27bae4c7032525
Paul Rosero <paul_rosero@yahoo.com>
2019-02-12 00:34:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 phrase = "I love you grandma!"
  if phrase
    "HUH?! SPEAK UP, SONNY!"
- elsif phrase.upcase
+ elsif phrase.upcase?
    "NO, NOT SINCE 1938!"
  else "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
